9024e24526e771f8c4a9920f2d7f90b7b2096ab1
hxx <hanxx@geovis.com.cn>
2023-05-02 13:38:22 +0800

    feat：webpack sourcemap  learning

---
+++ test-vue-debug/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png">
+  <HelloWorld msg="Welcome to Your Vue.js App"/>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+import { onMounted } from 'vue';
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  },
+  setup() {
+    onMounted(() => {
+      console.log('onMounted');
+    })
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
