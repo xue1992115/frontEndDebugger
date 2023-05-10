6f6ffb52a81f445b94004a648f5d7c7e394441e5
hxx <hanxx@geovis.com.cn>
2023-05-10 12:10:04 +0800

    feat: dom断点

--- react-debug/test-react-debug/src/App.js
+++ react-debug/test-react-debug/src/App.js
@@ -1,24 +1,17 @@
 import logo from "./logo.svg";
 import "./App.css";
+import { useEffect, useState } from 'react';
 
 function App() {
-  console.log('han');
+  const [name, setName ] = useState('han');
+  useEffect(() => {
+    setTimeout(() => {
+      setName('xiao')
+    }, 1000);
+  }, [name])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {name}
     </div>
   );
 }
